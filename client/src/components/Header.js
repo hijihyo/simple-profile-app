f8b85d93a28bdf9758dc1c63d118c24f3eea1e6c
Seunghyun Kim <shkim9533@korea.ac.kr>
2021-11-12 00:10:38 +0900

    Add and implemement, apply sidebar component

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,23 +1,39 @@
-import React from 'react';
-import AppBar from '@mui/material/AppBar';
-import Box from '@mui/material/Box';
-import Toolbar from '@mui/material/Toolbar';
-import Button from '@mui/material/Button';
-import IconButton from '@mui/material/IconButton';
-import Link from '@mui/material/Link';
-import MenuIcon from '@mui/icons-material/Menu';
+import React from 'react'
+import AppBar from '@mui/material/AppBar'
+import Box from '@mui/material/Box'
+import Toolbar from '@mui/material/Toolbar'
+import Button from '@mui/material/Button'
+import IconButton from '@mui/material/IconButton'
+import Link from '@mui/material/Link'
+import Drawer from '@mui/material/Drawer'
+import MenuIcon from '@mui/icons-material/Menu'
+
+import Sidebar from './Sidebar'
 
 const Header = () => {
+  const [sidebar, setSidebar] = React.useState(false)
+  const toggleDrawer = (value) => (event) => {
+    if (event.type === 'keydown' && (event.key === 'Tab' || event.key === 'Shift')) {
+      return
+    }
+    setSidebar(value)
+  }
+
   return (
     <Box sx={{ flexGrow: 1 }}>
       <AppBar position="static">
         <Toolbar>
-          <IconButton
-            size="large" edge="start" color="inherit"
-            aria-label="menu" sx={{ mr: 2 }}
-          >
-            <MenuIcon />
-          </IconButton>
+          <React.Fragment key="sidebar">
+            <IconButton
+              size="large" edge="start" color="inherit"
+              aria-label="menu" sx={{ mr: 2 }} onClick={toggleDrawer(true)}
+            >
+              <MenuIcon />
+            </IconButton>
+            <Drawer open={sidebar} onClose={toggleDrawer(false)}>
+              <Sidebar sidebar={ sidebar } toggleDrawer={ toggleDrawer }/>
+            </Drawer>
+          </React.Fragment>
           <Link
             variant="h5" href="/" underline="none"
             sx={{ color: "secondary.main", fontWeight: "bolder", textAlign: 'left', flexGrow: 1 }}
@@ -25,21 +41,21 @@ const Header = () => {
             Simple Profile App
           </Link>
           <Button
-            variant="outlined" href="/signin"
-            sx={{ width: '7vw', mx: '0.5vw', color: "secondary.main", fontWeight: "bold", borderColor: "secondary.main", "&:hover": { backgroundColor: "#eeeeee", borderColor: "secondary.main", boxShadow: 'none', } }}
+            variant="outlined" href="/signin" size="large"
+            sx={{ mx: '0.5vw', px: '1vw', color: "secondary.main", fontWeight: "bold", borderColor: "secondary.main", "&:hover": { backgroundColor: "#eeeeee", borderColor: "secondary.main", boxShadow: 'none', } }}
           >
             Sign In
           </Button>
           <Button
-            href="/signup"
-            sx={{ width: '7vw', mx: '0.5vw', color: "primary.main", fontWeight: "bold", backgroundColor: "secondary.main", "&:hover": { backgroundColor: "secondary.dark", boxShadow: 'none', } }}
+            href="/signup" size="large"
+            sx={{ mx: '0.5vw', px: '1vw', color: "primary.main", fontWeight: "bold", backgroundColor: "secondary.main", "&:hover": { backgroundColor: "secondary.dark", boxShadow: 'none', } }}
           >
             Sign Up
           </Button>
         </Toolbar>
       </AppBar>
     </Box>
-  );
+  )
 }
 
-export default Header;
+export default Header
---
+++ client/src/components/Sidebar.js
@@ -0,0 +1,55 @@
+import React from 'react'
+import Box from '@mui/material/Box'
+import List from '@mui/material/List'
+import Divider from '@mui/material/Divider'
+import ListItem from '@mui/material/ListItem'
+import ListItemIcon from '@mui/material/ListItemIcon'
+import ListItemText from '@mui/material/ListItemText'
+import ListItemButton from '@mui/material/ListItemButton'
+import SearchIcon from '@mui/icons-material/Search'
+import LibraryBooksIcon from '@mui/icons-material/LibraryBooks'
+import AccountCircleIcon from '@mui/icons-material/AccountCircle'
+import SettingsIcon from '@mui/icons-material/Settings'
+
+const Sidebar = ({ sidebar, toggleDrawer }) => {
+  return (
+    <Box
+      sx={{ width: 250 }}
+      role="presentation"
+      onClick={toggleDrawer(false)}
+      onKeyDown={toggleDrawer(false)}
+    >
+      <List>
+        <ListItem disablePadding>
+          <ListItemButton component="a" href="#">
+            <ListItemIcon><SearchIcon /></ListItemIcon>
+            <ListItemText primary="Search" />
+          </ListItemButton>
+        </ListItem>
+        <ListItem disablePadding>
+          <ListItemButton component="a" href="/profiles">
+            <ListItemIcon><LibraryBooksIcon /></ListItemIcon>
+            <ListItemText primary="Profiles" />
+          </ListItemButton>
+        </ListItem>
+      </List>
+      <Divider />
+        <List>
+          <ListItem disablePadding>
+            <ListItemButton component="a" href="/myprofile">
+              <ListItemIcon><AccountCircleIcon /></ListItemIcon>
+              <ListItemText primary="My Profile" />
+            </ListItemButton>
+          </ListItem>
+          <ListItem disablePadding>
+            <ListItemButton component="a" href="#">
+              <ListItemIcon><SettingsIcon /></ListItemIcon>
+              <ListItemText primary="Setting" />
+            </ListItemButton>
+          </ListItem>
+        </List>
+    </Box>
+  )
+}
+
+export default Sidebar
